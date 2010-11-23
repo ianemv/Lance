d57d219d4ba9a5e43afc0a780092c4f2bc585b23
Jared Mashburn <jmashburn@iwobble.com>
2010-11-22 22:18:22 -0700

    Adding Update

--- app/controllers/components/dineromail.php
+++ app/controllers/components/dineromail.php
@@ -24,9 +24,9 @@ class DineromailComponent extends Object {
 	function validate_ipn() {
 		if (!empty($this->xml)) {  
 			$notificacion = $this->xml->toArray();   
-			if (!empty($notificacion['notificacion'])) {  
+			if (!empty($notificacion['Notificacion'])) {  
 				$data = array();   
-		    	foreach ($notificacion['notificacion']['operaciones'] as $key => $value) { 
+		    	foreach ($notificacion['Notificacion']['Operaciones'] as $key => $value) { 
 					if (is_array($value)) {
 				   		foreach ($value as $val) {
 					    	$data[]['ID'] = $val['id'];
